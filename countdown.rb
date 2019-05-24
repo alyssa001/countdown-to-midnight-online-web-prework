f84e30ba51eaf616a24ad2d140cd8bc838035339
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:34:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+def countdown(num)
+  until num == 0 
+    puts "#{num} SECOND(S)!"
+    num -= 1 
+    if num == 0 
+       return "HAPPY NEW YEAR!"
+     end
+  end 
+end 
 
-def countdown
-end
+def countdown_with_sleep(num)
+  until num == 0 
+    puts "#{num} SECOND(S)!"
+     sleep(1)
+    num -= 1 
+    if num == 0 
+       return "HAPPY NEW YEAR!"
+     end
+  end 
+end 
+  
+ 
