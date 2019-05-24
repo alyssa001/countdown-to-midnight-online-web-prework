f74a2a184bee02a89cd5bf94f466eec8df3f1a31
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:33:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ end
 def countdown_with_sleep(num)
   until num == 0 
     puts "#{num} SECOND(S)!"
-     sleep(1.seconds)
+     sleep(1)
     num -= 1 
     if num == 0 
        return "HAPPY NEW YEAR!"
