a82c07060a5ca87733c8b275712e1f6a42324678
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:25:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+def countdown(num)
+  counter = 10 
+  while counter > 0 
+    counter -= 1 
+    puts "#{num SECOND(S)!"
+  end 
+  puts "HAPPY NEW YEAR!"
 end
