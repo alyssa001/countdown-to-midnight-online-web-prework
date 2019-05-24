8cf54f4a50b7b8b51c273591ae6a92e3c19b6ede
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:31:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,8 @@ def countdown(num)
   until num == 0 
     puts "#{num} SECOND(S)!"
     num -= 1 
-  if num == 0 
-    puts "HAPPY NEW YEAR!"
+    if num == 0 
+       puts "HAPPY NEW YEAR!"
+     end
   end 
 end 
